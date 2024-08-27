d71eacf29a832d759a92dcdd2deec8ed7d6a007f
lwstkhyl <1271943237@qq.com>
2024-08-27 11:17:07 +0800

    0827

--- test.js
+++ test.js
@@ -1,2 +1,6 @@
-let buf = Buffer.from('你好');
-console.log(buf); //<Buffer 69 65 6c 6c 6f>
+const fs = require('fs');
+const ws = fs.createWriteStream('./test.txt'); //创建写入流对象
+ws.write('追加内容1\n');
+ws.write('追加内容2\n');
+ws.write('追加内容3\n'); //追加写入
+ws.close(); //关闭通道（可省略）
